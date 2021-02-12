5db73590da5766b02d12564ce15d0aef091460cd
Sangam Bhattarai <bsangam4@gmail.com>
2021-02-12 19:46:38 +0545

    added question15

---
+++ data types/15.py
@@ -0,0 +1,4 @@
+#	Write a Python function to insert a string in the middle of a string.
+#	Sample function and result :
+#	insert_sting_middle('[[]]<<>>', 'Python') -> [[Python]]
+#	insert_sting_middle('{{}}', 'PHP') -> {{PHP}}
