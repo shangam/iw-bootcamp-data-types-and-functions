3d0d5a4b7a7425693c3b4a974c739a7a3f52793b
Sangam Bhattarai <bsangam4@gmail.com>
2021-02-12 19:11:02 +0545

    added question2

---
+++ data types/2.py
@@ -0,0 +1,8 @@
+#	Write a Python program to get a string made of the first 2 and the last 2 chars from a given string.
+#	If the string length is less than 2, return instead of the empty string.
+#	Sample String : 'Python'
+#	Expected Result : 'Pyon'
+#	Sample String : 'Py'
+#	Expected Result : 'PyPy'
+#	Sample String : ' w'
+#	Expected Result : Empty String
