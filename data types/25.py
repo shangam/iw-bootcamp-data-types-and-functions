cca081ee7600d52fb4f141c804d5df264ea5d9e8
Sangam Bhattarai <bsangam4@gmail.com>
2021-02-12 21:10:09 +0545

    added question25

---
+++ data types/25.py
@@ -0,0 +1,5 @@
+#	Write a Python program to check whether all dictionaries in a list are empty or not.
+#	Sample list : [{},{},{}]
+#	Return value : True
+#	Sample list : [{1,2},{},{}]
+#	Return value : False
