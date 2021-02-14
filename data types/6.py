16ec891cb741a8ae3ebc4af6ad8e0dbe3bbff403
Sangam Bhattarai <bsangam4@gmail.com>
2021-02-14 21:14:23 +0545

    updated question6

--- data types/6.py
+++ data types/6.py
@@ -5,3 +5,15 @@
 #	'The lyrics is poor!'
 #	Expected Result : 'The lyrics is good!'
 #	'The lyrics is poor!'
+
+def checks(x):
+    x1=x.find("not")
+    x2=x.find("poor")
+
+    if (x1>x2) and (x2>0):
+        x=x.replace(x[-x1],"good")
+        print(x)
+    else:
+        print(x)
+
+checks("the lyrics is not that poor !")
