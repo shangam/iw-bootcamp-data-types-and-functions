a0e3c62c00adeb34205d3dfe747c6d88413225e3
Sangam Bhattarai <bsangam4@gmail.com>
2021-02-14 21:09:06 +0545

    updated question4

--- data types/4.py
+++ data types/4.py
@@ -2,3 +2,9 @@
 #	by a space and swap the first two characters of each string.
 #	Sample String : 'abc', 'xyz'
 #	Expected Result : 'xyc abz'
+
+def swaps(x,y): 
+ x,y=y,x
+ print (x,y)
+
+swaps("abc","xyz")
