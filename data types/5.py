4203399abca8e58ceaf69285cf15ea9890a6d7c4
Sangam Bhattarai <bsangam4@gmail.com>
2021-02-14 21:10:58 +0545

    updated question5

--- data types/5.py
+++ data types/5.py
@@ -5,3 +5,17 @@
 #	Expected Result : 'abcing'
 #	Sample String : 'string'
 #	Expected Result : 'stringly'
+
+def changes(x):
+    a=len(x)
+    if(a<3):
+        return a
+    if (a>=3):
+        ch=x[-3:]
+
+        if (ch=="ing"):
+            return x+"ly"
+        else:
+            return x+"ing"         
+    
+print(changes("string"))
