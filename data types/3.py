e1d4f52384240cd572d6cf4271fd7d820eb135ce
Sangam Bhattarai <bsangam4@gmail.com>
2021-02-14 20:54:54 +0545

    updated question3

--- data types/3.py
+++ data types/3.py
@@ -2,3 +2,14 @@
 #	occurrences of its first char have been changed to '$', except the first char itself.
 #	Sample String : 'restart'
 #	Expected Result : 'resta$t'
+
+def change_char(str1):
+  char = str1[0]
+  str1 = str1.replace(char, '$')
+  print(str1)
+  
+  str1 = char + str1[1:]
+   
+  return str1
+
+print(change_char('restart'))
